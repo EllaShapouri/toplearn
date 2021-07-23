43694f05f811b03b77c9dabeae873391c9034d7d
EllaShapouri <ellashapouri@gmail.com>
2021-07-23 19:56:56 +0430

    add archive component

--- src/containers/TopLearn.jsx
+++ src/containers/TopLearn.jsx
@@ -4,6 +4,7 @@ import MainLayout from './../components/Layouts/MainLayout'
 import Course from './../components/Courses/Course';
 import Login from './../components/Login/Login';
 import Register from './../components/Register/Register';
+import Archive from './../components/Courses/Aechive';
 
 const TopLearn = () => {
     return (
@@ -11,6 +12,7 @@ const TopLearn = () => {
             <Switch>
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/archive" component={Archive} />
                 <Route path="/" exact component={Course} />
                 {/* <Course /> */}
             </Switch>
