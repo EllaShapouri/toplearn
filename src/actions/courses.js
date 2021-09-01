6e396b4e8f9023281aad6cfdda124f57ec353beb
EllaShapouri <ellashapouri@gmail.com>
2021-09-01 12:13:45 +0430

    courses action

---
+++ src/actions/courses.js
@@ -0,0 +1,8 @@
+import { getCourses } from "./../Services/coursesServices";
+
+export const allCourses = () => {
+  return async (dispatch) => {
+    const { data } = await getCourses();
+    await dispatch({ type: "INIT", payload: data.courses });
+  };
+};
