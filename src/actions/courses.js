1644ae81c324b47253de1347af7e6136552a02d0
EllaShapouri <ellashapouri@gmail.com>
2021-09-23 18:52:42 +0330

    add edit course action

--- src/actions/courses.js
+++ src/actions/courses.js
@@ -1,5 +1,9 @@
 import { successMessage } from "../utils/toast";
-import { getCourses, newCourse } from "./../Services/coursesServices";
+import {
+    getCourses,
+    newCourse,
+    updateCourse,
+} from "./../Services/coursesServices";
 
 export const allCourses = () => {
     return async (dispatch) => {
@@ -16,5 +20,33 @@ export const createNewCourse = (course) => {
             type: "ADD_COURSE",
             payload: [...getState().courses, data.course],
         });
+    };
+};
+
+export const editCourse = (courseId, updatedCourse) => {
+    return async (dispatch, getState) => {
+        const courses = [...getState().courses];
+        const prevCourses = [...courses];
+        const courseIndex = courses.findIndex(
+            (course) => courseId === course._id
+        );
+
+        let thisCourse = courses[courseIndex];
+        thisCourse = { ...Object.fromEntries(updatedCourse) };
+        courses[courseIndex] = thisCourse;
+        try {
+            await dispatch({ type: "UPDATE_COURSE", payload: [...courses] });
+            const { data, status } = await updateCourse(
+                courseId,
+                updatedCourse
+            );
+            if (status === 200) successMessage("دوره با موفقیت ویرایش شد .");
+            console.log(data);
+        } catch (error) {
+            await dispatch({
+                type: "UPDATE_COURSE",
+                payload: [...prevCourses],
+            });
+        }
     };
 };
