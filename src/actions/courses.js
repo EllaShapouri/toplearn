83a2dccef42d711b184c39710d26df6f8301f3ef
EllaShapouri <ellashapouri@gmail.com>
2021-09-23 16:08:52 +0330

    create action for new course

--- src/actions/courses.js
+++ src/actions/courses.js
@@ -1,8 +1,20 @@
-import { getCourses } from "./../Services/coursesServices";
+import { successMessage } from "../utils/toast";
+import { getCourses, newCourse } from "./../Services/coursesServices";
 
 export const allCourses = () => {
-  return async (dispatch) => {
-    const { data } = await getCourses();
-    await dispatch({ type: "INIT", payload: data.courses });
-  };
+    return async (dispatch) => {
+        const { data } = await getCourses();
+        await dispatch({ type: "INIT", payload: data.courses });
+    };
+};
+
+export const createNewCourse = (course) => {
+    return async (dispatch, getState) => {
+        const { data, status } = await newCourse(course);
+        if (status === 201) successMessage("دوره با موفقیت ساخته شد");
+        await dispatch({
+            type: "ADD_COURSE",
+            payload: [...getState().courses, data.course],
+        });
+    };
 };
