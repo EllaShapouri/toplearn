6d1e07cfedb760a1e31dfc520784067114216de9
EllaShapouri <ellashapouri@gmail.com>
2021-09-23 16:07:40 +0330

    create http service for post new/delete/update course

--- src/Services/coursesServices.js
+++ src/Services/coursesServices.js
@@ -8,3 +8,15 @@ export const getCourses = () => {
 export const getCourse = (courseId) => {
     return http.get(`${config.toplearnapi}/api/course/${courseId}`);
 };
+
+export const newCourse = (course) => {
+    return http.post(`${config.toplearnapi}/api/course`, course);
+};
+
+export const deleteCourse = (courseId) => {
+    return http.delete(`${config.toplearnapi}/api/course/${courseId}`);
+};
+
+export const updateCourse = (courseId, course) => {
+    return http.put(`${config.toplearnapi}/api/course/${courseId}`, course);
+};
