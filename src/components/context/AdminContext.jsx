e6c6e151163e78da7db7fc0a8420d98cac245c16
EllaShapouri <ellashapouri@gmail.com>
2021-09-23 16:11:11 +0330

    use new course dialog in this component

--- src/components/context/AdminContext.jsx
+++ src/components/context/AdminContext.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { dashContext } from "./DashContext";
 import { paginate } from "./../../utils/paginate";
+import NewCourseDialog from "./../admin/dialogs/NewCourseDialog";
 
 const AdminContext = ({ courses, children }) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const [perPage, setperPage] = useState(5);
-
+    const [perPage] = useState(5);
+    const [newCourseDialog, setNewCourseDialog] = useState(false);
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
+
+    const openNewCourseDialog = () => setNewCourseDialog(true);
+
+    const closeNewCourseDialog = () => setNewCourseDialog(false);
 
     const courseData = paginate(courses, currentPage, perPage);
 
@@ -19,8 +24,13 @@ const AdminContext = ({ courses, children }) => {
                 perPage,
                 handlePageChange,
                 courseData,
+                openNewCourseDialog,
             }}
         >
+            <NewCourseDialog
+                showDialog={newCourseDialog}
+                closeDialog={closeNewCourseDialog}
+            />
             {children}
         </dashContext.Provider>
     );
